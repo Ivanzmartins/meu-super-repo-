6a4c0e0885917099243c456b419e764c6f5821d1
Ivan <ivanzmartins@gmail.com>
2022-05-13 13:11:37 -0300

    Exercicio 7

--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/exercicio1.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -1,4 +1,5 @@
 //EXERCICIO 1
+
 const a=10;
 const b=3;
 
@@ -16,6 +17,7 @@ else {
 }
 
 //EXERCICIO 3
+
 const a=5, b=11, c= 15;
 
 if(a>b && a>c){
@@ -30,6 +32,7 @@ else{
 
 
 //EXERCICIO 4
+
 const num=-0;
 
 if(num>0){
@@ -41,6 +44,65 @@ else if(num<0){
 else{
     console.log("Zero")
 }
-//EXERCICIO 5
+//EXERCICIO 5 
 
-//EXERCICIO 6
+const a=90, b=50, c=50;
+
+if(a<0 || b<0 || c<0){
+    console.log("Um ou mais angulos inválidos");
+}
+console.log(a+b+c==180)
+
+//EXERCICIO 6   
+
+let piece = "casa";
+piece = piece.toLowerCase();
+
+switch (piece){
+    case "pawn":
+        console.log("Anda somente uma casa à frente, exceto no primeiro movimento, que pode andar duas");
+         break;
+    case "bishop":
+        console.log("Anda nas diagonais, quantas casas for possível");
+        break;
+    case "knight":
+        console.log("Anda em L");
+        break;
+    case "rook":
+        console.log("Anda quantas casas possível em linhas retas");
+        break;
+    case "queen":
+        console.log("Anda quantas casas quiser, seja diagonal quanto em linhas retas");
+        break;
+    case "king":
+        console.log("Uma casa em qualquer direção");
+    default:
+        console.log("peça inválida")
+}
+
+//EXERCÍCIO 7
+
+const nota = 10;
+
+if (nota<0 || nota>100){
+    console.log("erro");
+}
+else if( nota< 50){
+    console.log("Nota F");
+    }
+else if(nota >= 50 && nota < 60){
+    console.log("Nota E");
+    }
+
+else if(nota >=60 && nota < 70){
+    console.log("Nota D");
+}
+else if (nota>=70 && nota<80){
+    console.log("Nota C");
+}
+else if (nota>=80 && nota<90){
+    console.log("Nota B");
+}
+else{
+    console.log("Nota A");
+}
