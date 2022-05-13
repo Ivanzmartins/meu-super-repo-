e14572dd720a241fb95ff0bb505bcdef15b766a3
Ivan <ivanzmartins@gmail.com>
2022-05-13 16:34:48 -0300

    Exercicio 6

---
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2/exericios.js
@@ -0,0 +1,46 @@
+//exercicio 1 a 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma=0;
+let media;
+for (index=0; index<numbers.length; index++){
+    soma=soma+numbers[index];
+    }
+media=soma/numbers.length;
+console.log(media);
+if (media>20){
+    console.log("valor maior que 20");
+}
+else{
+    console.log("Valor menor ou igual a 20");
+}
+
+//exercicio 5
+
+let numbers = [5, 9, 3, 19, 110, 8, 100, 2, 35, 27];
+let maiorValor=0;
+
+for (index=0; index<numbers.length; index++){
+    if (numbers[index]>maiorValor){
+        maiorValor=numbers[index];
+    }
+}
+console.log(maiorValor);
+
+//exercicio 6
+let numbers = [5, 9, 3, 19, 110, 8, 100, 2, 35, 27];
+let impar=0;
+
+for (index=0; index<numbers.length; index++){
+    if( numbers[index]%2>0){
+        impar++;
+    }
+}
+if(impar>0){
+    console.log("Existem "+impar+" números ímpares");
+}
+else{
+    console.log("Não existem números ímpares")
+}
+
+//exercicio
---
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2/fixação.js
@@ -0,0 +1,17 @@
+//Arrays
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
+
+//FOR
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (var index=0; index<groceryList.length; index++  ){
+    console.log(groceryList[index]);
+
+}
+
+//FOR OF
+
