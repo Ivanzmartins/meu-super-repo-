2f3122140162d44b28606387ac9f202dc39b226c
Ivan <ivanzmartins@gmail.com>
2022-05-13 09:46:36 -0300

    Organiza todos os exercícios em um unico arquivo

--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/exercicio1.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -1,5 +1,46 @@
+//EXERCICIO 1
 const a=10;
 const b=3;
 
 console.log(a%b);
 
+//EXERCICIO 2
+
+const a=10, b=7;
+
+if(a>b){
+    console.log(a);
+}
+else {
+    console.log(b);
+}
+
+//EXERCICIO 3
+const a=5, b=11, c= 15;
+
+if(a>b && a>c){
+    console.log(a);
+}
+else if(b>a && b>c){
+    console.log(b);
+}
+else{
+    console.log(c)
+}
+
+
+//EXERCICIO 4
+const num=-0;
+
+if(num>0){
+    console.log("Positive");
+}
+else if(num<0){
+    console.log("Negative");
+}
+else{
+    console.log("Zero")
+}
+//EXERCICIO 5
+
+//EXERCICIO 6
--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-1-javascript-primeiros-passos/exercicio2.js
+++
@@ -1,8 +0,0 @@
-const a=10, b=7;
-
-if(a>b){
-    console.log(a);
-}
-else {
-    console.log(b);
-}
