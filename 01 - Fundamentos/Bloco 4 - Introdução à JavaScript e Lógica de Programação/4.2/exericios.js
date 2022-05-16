1136aa107e7ee831e7492e0887656218fe559e11
Ivan <ivanzmartins@gmail.com>
2022-05-16 15:32:05 -0300

    exercício 7

--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2/exericios.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2/exericios.js
@@ -1,46 +1,61 @@
-//exercicio 1 a 4
+// //exercicio 1 a 4
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma=0;
-let media;
-for (index=0; index<numbers.length; index++){
-    soma=soma+numbers[index];
-    }
-media=soma/numbers.length;
-console.log(media);
-if (media>20){
-    console.log("valor maior que 20");
-}
-else{
-    console.log("Valor menor ou igual a 20");
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma=0;
+// let media;
+// for (index=0; index<numbers.length; index++){
+//     soma=soma+numbers[index];
+//     }
+// media=soma/numbers.length;
+// console.log(media);
+// if (media>20){
+//     console.log("valor maior que 20");
+// }
+// else{
+//     console.log("Valor menor ou igual a 20");
+// }
 
-//exercicio 5
+// //exercicio 5
 
+// let numbers = [5, 9, 3, 19, 110, 8, 100, 2, 35, 27];
+// let maiorValor=0;
+
+// for (index=0; index<numbers.length; index++){
+//     if (numbers[index]>maiorValor){
+//         maiorValor=numbers[index];
+//     }
+// }
+// console.log(maiorValor);
+
+// //exercicio 6
+// let numbers = [5, 9, 3, 19, 110, 8, 100, 2, 35, 27];
+// let impar=0;
+
+// for (index=0; index<numbers.length; index =+ 1){
+//     if( numbers[index]%2>0){
+//         impar += 1
+//     }
+// }
+// if(impar>0){
+//     console.log("Existem "+impar+" números ímpares");
+// }
+// else{
+//     console.log("Não existem números ímpares")
+// }
+
+//exercicio 7
 let numbers = [5, 9, 3, 19, 110, 8, 100, 2, 35, 27];
-let maiorValor=0;
+let lowValue=0;
 
 for (index=0; index<numbers.length; index++){
     if (numbers[index]>maiorValor){
         maiorValor=numbers[index];
     }
 }
-console.log(maiorValor);
+console.log(lowValue);
 
-//exercicio 6
-let numbers = [5, 9, 3, 19, 110, 8, 100, 2, 35, 27];
-let impar=0;
+4
 
-for (index=0; index<numbers.length; index++){
-    if( numbers[index]%2>0){
-        impar++;
-    }
-}
-if(impar>0){
-    console.log("Existem "+impar+" números ímpares");
-}
-else{
-    console.log("Não existem números ímpares")
-}
 
-//exercicio
+newArray = newArray/2
+
