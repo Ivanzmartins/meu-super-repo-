cf6c9c765928f73323b732190b29c95ca27054b0
Ivan <ivanzmartins@gmail.com>
2022-05-16 19:00:11 -0300

    Adiciona aula ao vivo e exercício 3 de fixação

---
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/aulaaovivo.js
@@ -0,0 +1,16 @@
+let linhaRainha = 0, colunaRainha = 0;
+let linhaBispo = 3, colunaBispo = 5;
+let podeAtacar = false;
+
+//Verifica se está na mesma linha
+if (linhaRainha === linhaBispo || colunaRainha === colunaBispo){
+    podeAtacar = true;
+}
+
+//Verifica se está na mesma diagonal
+
+for (let passos = 1; passos <= 7; passos=+1){
+    let linhaRainhaClone = linhaRainha + passos;
+    if(linhaRainhaClone === linhaBispo && coli)
+}
+
--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
@@ -8,8 +8,30 @@
 
 // Exercicio 2
 
-let word = 'oto come mocoto', reverse="";
-for ( let i=word.length-1; i>=0; i-=1){
-    reverse+=word[i]
-}
-console.log(reverse)
+// let word = 'casa', reverse="";
+// for ( let i=word.length-1; i>=0; i-=1){
+//     reverse+=word[i];
+//     console.log(reverse);
+
+// }
+// console.log(reverse)
+
+// //Exercicio 3
+// let array = ['java', 'javascript', 'python', 'html', 'css']
+// let longestWord = array[0];
+// let smallestWord = array[0];
+
+// for(let i = 0; i < array.length; i+=1){
+//     if( array[i].length > longestWord.length){
+//         longestWord = array[i];
+
+//     }
+//     if(array[i].length < smallestWord.length){
+//         smallestWord = array[i];
+//     }
+// }
+// console.log("A maior palavra é: " +longestWord,".");
+// console.log("A menor palavra é: " +smallestWord+".")
+
+//Exercício 4
+
