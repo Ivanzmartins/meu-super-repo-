1c3d1baf23e2636a6b73d86d8adbf976e01ac7cc
Ivan <ivanzmartins@gmail.com>
2022-05-16 16:37:07 -0300

    exercicio 2

--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
@@ -1,6 +1,15 @@
-let n=4, resultado=1;
+// // Exercício 1
+// let n=10, resultado=1;
 
-for ( let i=n; i>=1; i-=1){
-    resultado=resultado*i
+// for ( let i=n; i>=1; i-=1){
+//     resultado=resultado*i
+// }
+// console.log("O fatorial de " +n+" é: "+resultado)
+
+// Exercicio 2
+
+let word = 'oto come mocoto', reverse="";
+for ( let i=word.length-1; i>=0; i-=1){
+    reverse+=word[i]
 }
-console.log(resultado)
+console.log(reverse)
