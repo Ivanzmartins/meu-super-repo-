c70896f65e980f3c53f4be738c3b600aa19b6e4a
Ivan <ivanzmartins@gmail.com>
2022-05-17 16:54:12 -0300

    mais exercicios

--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-4-javascript-objetos-e-funcoes/fixacao.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-4-javascript-objetos-e-funcoes/fixacao.js
@@ -78,3 +78,36 @@
 // console.log("A jogadora " + player.name + ' ' +player.lastName+ " foi eleita a melhor do mundo por " + player.bestInTheWorld.length +" vezes."  )
 // console.log("A jogadora " + player.name + ' ' +player.lastName+ " possui " + player.medals.golden +" medalhas de ouro e " + player.medals.silver + " medalhas de prata."  )
 
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+//Fixação for/in for/of
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+//   for (let index in names){
+//       console.log("Olá " + names[index])
+//   }
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  for (let index in car){
+      console.log(index, car[index])
+  }
