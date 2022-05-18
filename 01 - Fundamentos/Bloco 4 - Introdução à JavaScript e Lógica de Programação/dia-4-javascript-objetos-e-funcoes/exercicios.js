a87881cf1b0b8288659f0d0b465b778c8f39d118
Ivan <ivanzmartins@gmail.com>
2022-05-18 11:35:42 -0300

    exercicio 5

--- 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ 01 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,16 +1,28 @@
-// Exercicios 1 a 5
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// // Exercicios 1 a 5
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'sim'
+//   };
+//   let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'sim'
+//   };
 
-//console.log('Bem vinda ' + info.personagem);
 
-info['recorrente'] = 'Sim' // adiciona recorrente ao objeto
+// //console.log('Bem vinda ' + info.personagem);
 
-//console.log('recorrente? ' + info.recorrente)
+// // info['recorrente'] = 'Sim' // adiciona recorrente ao objeto
 
-for ( let index in info){ 
-    console.log(index)
-}
+// //console.log('recorrente? ' + info.recorrente)
+
+// for ( let index in info){ 
+//     if (index === 'recorrente' && info[index] === 'sim' && info2[index] === 'sim'){
+//         console.log('Ambos recorrentes')
+//     }else{
+//         console.log(info[index]+' e ' + info2[index])
+//     } 
+// }
