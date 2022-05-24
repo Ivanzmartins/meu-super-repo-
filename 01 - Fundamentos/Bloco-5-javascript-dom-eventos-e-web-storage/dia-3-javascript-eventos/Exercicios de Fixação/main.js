237d96d27fc1f717395369d3bea3e847d34174e2
Ivan <ivanzmartins@gmail.com>
2022-05-24 14:59:55 -0300

    Finalmente termina os exercicios de fixação

--- 01 - Fundamentos/Bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Exercicios de Fixação/main.js
+++ 01 - Fundamentos/Bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Exercicios de Fixação/main.js
@@ -7,38 +7,61 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?  A primeira caixa tem uma classe que não está atribuída às outras
+// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+ function changeClass (element){
+   const classTech = document.querySelector('.tech');
+   classTech.classList.remove('tech');
+   element.target.classList.add('tech');
+   input.value = '';
+ } 
+
+firstLi.addEventListener('click', changeClass); // adiciona um "ouvidor" de eventos aos li's
+secondLi.addEventListener('click', changeClass);
+thirdLi.addEventListener('click', changeClass); 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+function textoTech (){
+  const techBox = document.querySelector('.tech');
+  techBox.innerText = input.value; 
+}
+input.addEventListener('input', textoTech);
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+function redirect(){
+  window.location.replace('https://www.w3schools.com/howto/howto_js_redirect_webpage.asp') 
+}
+
+myWebpage.addEventListener('dblclick', redirect);
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
-// Segue abaixo um exemplo do uso de event.target:
-
-let testaMouseOverPrimeiraCaixa = document.getElementById('first-li');
-testaMouseOverPrimeiraCaixa.addEventListener("mouseover", mouseEmCima);
-function mouseEmCima(){
-    console.log('testaMouseOverPrimeiraCaixa')
+function mouseMudaCor(){
+  if(myWebpage.style.color === 'white'){
+    myWebpage.style.color = 'blue';
+  } else 
+  myWebpage.style.color = 'white'; 
 }
+myWebpage.addEventListener('mouseover', mouseMudaCor)
+// Segue abaixo um exemplo do uso de event.target:
 
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Clicou 2x'; 
+  event.target.innerText = 'Opção reiniciada';
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
 
 firstLi.addEventListener('dblclick', resetText);
+secondLi.addEventListener('dblclick', resetText);
+thirdLi.addEventListener('dblclick', resetText)
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
