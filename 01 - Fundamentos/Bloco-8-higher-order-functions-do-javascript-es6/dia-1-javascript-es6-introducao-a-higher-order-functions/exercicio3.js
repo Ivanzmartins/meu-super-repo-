3ad0c1c9db223c92caa9db9316456c73b83e805c
Ivanzmartins <ivanzmartins@gmail.com>
2022-06-23 18:11:34 -0300

    Inicio exerc bônus

--- 01 - Fundamentos/Bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
+++ 01 - Fundamentos/Bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
@@ -4,7 +4,7 @@ const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 const checkAnswers = (gabarito, respostasAluno) => {
   if (gabarito === respostasAluno) {
     return 1;
-  }else if (respostasAluno === 'N.A') {
+  }if (respostasAluno === 'N.A') {
     return 0;
   } else {
     return -0.5;
@@ -15,7 +15,6 @@ const avaliadorTotal = (gabarito, respostasAluno, callback) => {
   let notaFinal = 0;
   for (let index = 0; index < gabarito.length; index += 1){
     const retornoCallback = callback(gabarito[index], respostasAluno[index]);
-    console.log(retornoCallback);
     notaFinal += retornoCallback;
   }
   return notaFinal;
